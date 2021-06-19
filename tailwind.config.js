36e0fcc434cc2b04d0a3d151875b30b489241029
iotti0 <82509628+iotti0@users.noreply.github.com>
2021-06-19 20:18:47 +0700

    min-width fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,11 @@ module.exports = {
   theme: {
     extend: {
       fontFamily: {
-        sans: ['inter var', 'ui-sans-serif', 'system-ui', '-apple-system',],
-      }
+        sans: ['inter var', 'ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont', 'Segoe-UI', 'Arial', 'sans-serif'],
+      },
+      maxWidth: {
+        xs: '21rem',
+      },
     },
   },
   variants: {
